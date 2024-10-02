e405b56ee08441a33a7a621e290f66dff720ffce
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-02 19:01:10 +0200

    first-commit

---
+++ BD/categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO `categories` (`id`, `nomcategorie`, `imagecategorie`, `created_at`, `updated_at`) VALUES
+(2, 'électroniques', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749242/images/lenouv.jpg.jpg', NULL, NULL),
+(3, 'Téléphone & Tablette', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749354/images/Telephones-tablettes.jpg.jpg', NULL, NULL),
+(4, 'Informatique', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749154/images/Informatique.jpg.jpg', NULL, NULL),
+(5, 'Mode', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658747406/images/Mode-HommeJ.jpg.jpg', NULL, NULL),
+(6, 'Articles de sport', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749202/images/sport4.jpg.jpg', NULL, NULL),
+(7, 'Santé & Beauté', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749261/images/Sante-beaute.jpg.jpg', NULL, NULL),
+(8, 'Superette', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658749330/images/Superettes.jpg.jpg', NULL, NULL),
+(9, 'jeux vidéos', 'http://res.cloudinary.com/iset-sfax/image/upload/v1658593524/images/jeuxvideo.jpg.jpg', NULL, NULL);
+
