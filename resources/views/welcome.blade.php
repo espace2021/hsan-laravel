fcb52b60bfbed5384bf28e2caa017b69cb7fac06
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-25 15:54:09 +0200

    dist-commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 <head>
     <title>Mon application Laravel</title>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link href="{{ secure_asset('frontend/assets/index-BxdMoss1.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('frontend/assets/index-CjTAN1GL.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div>
-    <script type="module" src="{{ secure_asset('frontend/assets/index-ha1XktLW.js') }}"></script>
+    <script type="module" src="{{ secure_asset('frontend/assets/index-6JVNwdEB.js') }}"></script>
 </body>
 </html>
